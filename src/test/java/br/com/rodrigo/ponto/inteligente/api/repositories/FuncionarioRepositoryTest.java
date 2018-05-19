0acdfb90c3f0d66a85a6d87315138b3a5b31438b
rodJeronimo <rodrigo.jeronimo@msn.com>
2018-05-19 13:05:54 -0300

    Correção dos testes Empresa Repository

--- src/test/java/br/com/rodrigo/ponto/inteligente/api/repositories/FuncionarioRepositoryTest.java
+++ src/test/java/br/com/rodrigo/ponto/inteligente/api/repositories/FuncionarioRepositoryTest.java
@@ -1,5 +1,30 @@
 package br.com.rodrigo.ponto.inteligente.api.repositories;
 
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.rodrigo.ponto.inteligente.api.models.Empresa;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
 public class FuncionarioRepositoryTest {
 
+	@Autowired
+	private FuncionarioRepository funcionarioRepository;
+	
+	@Autowired
+	private EmpresaRepository empresaRepository;
+	
+	private static final String EMAIL = "email@email.com";
+	private static final String CPF = "24291173474";
+	
+	@Before
+	public void setUp() {
+		Empresa empresa = new Empresa();
+	}
 }
